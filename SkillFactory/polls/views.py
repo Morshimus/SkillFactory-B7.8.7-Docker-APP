5f0a4bc47789376e9c9c768a26996f408df3f6ef
Daniel Dalavurak <morsh110792@yahoo.com>
2023-02-09 14:09:41 +0300

    OK

--- SkillFactory/polls/views.py
+++ SkillFactory/polls/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-
-# Create your views here.
+    template = loader.get_template('polls/index.html')
+    return HttpResponse(template.render())
